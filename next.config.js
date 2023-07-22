7f2414738d7c5ead528b2dd93adbd1348bf9c6f9
Fedulov Anton, P <live.stone.anton@gmail.com>
2023-07-23 01:06:18 +0500

    fix: i18n

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
-const { i18n } = require('./next-i18next.config');
+// const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  i18n,
+  // https://github.com/vercel/next.js/issues/46622
+  // i18n,
 };
 
 module.exports = nextConfig;
