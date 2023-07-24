913acd42c6a211ec6591bf0440e838c210e14cef
Fedulov Anton, P <live.stone.anton@gmail.com>
2023-07-24 18:12:52 +0500

    fix: removed apollo nextjs

--- src/lib/apollo/client.ts
+++ src/lib/apollo/client.ts
@@ -1,6 +1,5 @@
 import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
-import { SSRMultipartLink } from '@apollo/experimental-nextjs-app-support/ssr';
 
 const GRAPHQL_ENDPOINT = process.env.NEXT_PUBLIC_STRAPI_ENDPOINT_GRAPHQL;
 
@@ -20,17 +19,13 @@ const onErrorLink = onError((error) => {
   console.error({ networkError: error });
 });
 
-const ssrLink = new SSRMultipartLink({
-  stripDefer: true,
-});
-
 export const getClient = () => {
   const isServer = typeof window === 'undefined';
   const links = [onErrorLink, authMiddleware, httpLink];
   if (!client || isServer) {
     client = new ApolloClient({
       cache: new InMemoryCache(),
-      link: ApolloLink.from(isServer ? [ssrLink, ...links] : links),
+      link: ApolloLink.from(links),
     });
   }
 
